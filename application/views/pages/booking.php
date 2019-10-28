4a1ce239c050fd4cddce50e9fed1dc0031bb9276
OJDylan <dooi2142@gmail.com>
2019-10-28 14:56:22 +0800

    Spelling error

--- application/views/pages/booking.php
+++ application/views/pages/booking.php
@@ -64,25 +64,25 @@
   $(document).ready(function () {
     $('#basketball a').on('click', function () {
       var time= ($(this).text());
-      document.getElementById("booking_details").innerHTML = "Are you sure you wanna book at this time: " + time;
+      document.getElementById("booking_details").innerHTML = "Are you sure you want to book at this time: " + time;
       document.getElementById("modal_title").innerHTML = "Basketball Court";
     });
 
     $('#badminton a').on('click', function () {
       var time= ($(this).text());
-      document.getElementById("booking_details").innerHTML = "Are you sure you wanna book at this time: " + time;
+      document.getElementById("booking_details").innerHTML = "Are you sure you want to book at this time: " + time;
       document.getElementById("modal_title").innerHTML = "Badminton Court";
     });
 
     $('#mac a').on('click', function () {
       var time= ($(this).text());
-      document.getElementById("booking_details").innerHTML = "Are you sure you wanna book at this time: " + time;
+      document.getElementById("booking_details").innerHTML = "Are you sure you want to book at this time: " + time;
       document.getElementById("modal_title").innerHTML = "Mac Studio";
     });
 
     $('#bb a').on('click', function () {
       var time= ($(this).text());
-      document.getElementById("booking_details").innerHTML = "Are you sure you wanna book at this time: " + time;
+      document.getElementById("booking_details").innerHTML = "Are you sure you want to book at this time: " + time;
       document.getElementById("modal_title").innerHTML = "Black Box";
     });
   });
