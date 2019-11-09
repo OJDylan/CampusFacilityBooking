c15dcc67352f1d25dfbff71a10a2f6979b00bf77
OJDylan <dooi2142@gmail.com>
2019-11-09 15:40:16 +0800

    Working date/month counter

--- application/views/pages/booking.php
+++ application/views/pages/booking.php
@@ -7,40 +7,11 @@
 <h1><p>&nbsp</p>Step 2: Select Date</h1>
 <div class="container">
   <div class="form-group">
-    <label class="col-form-label" for="dateSelect">Date</label>
-      <select style="width: 10%" class="form-control" id="dateSelect">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-        <option>6</option>
-        <option>7</option>
-        <option>8</option>
-        <option>9</option>
-        <option>10</option>
-        <option>11</option>
-        <option>12</option>
-        <option>13</option>
-        <option>14</option>
-        <option>15</option>
-        <option>16</option>
-        <option>17</option>
-        <option>18</option>
-        <option>19</option>
-        <option>20</option>
-        <option>21</option>
-        <option>22</option>
-        <option>23</option>
-        <option>24</option>
-        <option>25</option>
-        <option>26</option>
-        <option>27</option>
-        <option>28</option>
-        <option>29</option>
-        <option>30</option>
-        <option>31</option>
-      </select>
+    <p id="month"></p>
+    <label class="col-form-label" for="days">Date</label>
+    <select style="width: 10%" class="form-control" id="days">
+      <option>-</option>
+    </select>
   </div>
 </div>
 
--- application/views/templates/scripts.php
+++ application/views/templates/scripts.php
@@ -1,5 +1,6 @@
 <script>
   $(document).ready(function () {
+
     $('#basketball a').on('click', function () {
       var time= ($(this).text());
       document.getElementById("booking_details").innerHTML = "Are you sure you want to book at this time: " + time;
@@ -41,6 +42,60 @@
       document.getElementById("booking_details").innerHTML = "Are you sure you want to book at this time: " + time;
       document.getElementById("modal_title").innerHTML = "Learning Pod";
     });
-
   });
+</script>
+
+<script>
+  var month;
+    switch (new Date().getMonth()) {
+      case 0:
+        month = "January";
+        break;
+      case 1:
+        month = "February";
+        break;
+      case 2:
+        month = "March";
+        break;
+      case 3:
+        month = "April";
+        break;
+      case 4:
+        month = "May";
+        break;
+      case 5:
+        month = "June";
+        break;
+      case 6:
+        month = "July";
+        break;
+      case 7:
+        month = "August";
+        break;
+      case 8:
+        month = "September";
+        break;
+      case 9:
+        month = "October";
+        break;
+      case 10:
+        month = "November"
+        break;
+      case 11:
+        month = "December";  
+    }
+    document.getElementById("month").innerHTML = month;
+</script>
+
+<script>
+  var year = new Date().getFullYear();
+  var month = new Date().getMonth();
+  var days = new Date(year, month + 1, 0).getDate();
+  var result = '';
+
+  for(var i=1; i<=days; i++){
+    result += '<option>'+i+'</option>';
+  }
+
+  $('select#days').empty().html(result);
 </script>
